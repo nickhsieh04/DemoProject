802dde829ecb9d5c031b3a33f8364404de3cb210
nickhsieh04 <nick.hsieh04@gmail.com>
2016-07-01 22:53:08 +0800

    first commit

---
+++ WebA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebA.WebApiApplication" Language="C#" %>
---
+++ WebApi/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApi.Model;
+
+namespace WebApi
+{
+    [RoutePrefix("api")]
+    public class DemoController : ApiController
+    {
+        [HttpGet]
+        public List<Demo> Get()
+        {
+            List<Demo> retVal = new List<Demo>() { 
+                new Demo() { Id = 1, Name = "Andy" },
+                new Demo() { Id = 2, Name = "Nick" },
+                new Demo() { Id = 3, Name = "Allen" },
+            };
+
+            return retVal;
+        }
+    }
+}
