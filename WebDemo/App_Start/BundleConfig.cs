cfbedea571640e1b6438a072a40f1ab1a55943c3
nickhsieh04 <nick.hsieh04@gmail.com>
2016-07-08 23:54:31 +0800

    網站套版

--- WebDemo/App_Start/BundleConfig.cs
+++ WebDemo/App_Start/BundleConfig.cs
@@ -25,6 +25,37 @@ namespace WebDemo
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/charisma.css").Include(
+                "~/Content/bootstrap-cerulean.min.css",
+                //"~/Content/bootstrap-cyborg.min.css",
+                //"~/Content/bootstrap-darkly.min.css",
+                //"~/Content/bootstrap-lumen.min.css",
+                //"~/Content/bootstrap-simplex.min.css",
+                //"~/Content/bootstrap-slate.min.css",
+                //"~/Content/bootstrap-spacelab.min.css",
+                //"~/Content/bootstrap-united.min.css",
+                "~/Content/charisma-app.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/bowser_components").Include(
+                "~/Scripts/bower_components/chosen.jquery.min.js",
+                "~/Scripts/bower_components/jquery.colorbox.js",
+                "~/Scripts/bower_components/moment.js",
+                "~/Scripts/bower_components/fullcalendar.min.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/charisma").Include(
+                "~/Scripts/jquery.autogrow-textarea.js",
+                "~/Scripts/jquery.cookie.js",
+                "~/Scripts/jquery.dataTables.min.js",
+                "~/Scripts/jquery.history.js",
+                "~/Scripts/jquery.iphone.toggle.js",
+                "~/Scripts/jquery.noty.js",
+                "~/Scripts/jquery.raty.min.js",
+                "~/Scripts/jquery.uploadify-3.1.min.js",
+                "~/Scripts/charisma.js"
+                ));
         }
     }
 }
