48fe3f8dd02f6176605efe24d026aef1b6847675
nick_hsieh <nick_hsieh@cht.com.tw>
2016-08-05 16:24:15 +0800

    temp

---
+++ WebDemo/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDemo.Models;
+
+namespace WebDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult FacebookLogin(FacebookLoginModel model)
+        {
+            Session["uid"] = model.uid;
+            Session["accessTolen"] = model.accessToken;
+
+            return Json(new { success = true });
+        }
+    }
+}
