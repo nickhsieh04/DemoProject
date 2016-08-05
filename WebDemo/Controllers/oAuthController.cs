9d7f1ffce0e1b747d7579cf65e3c216ad2d07679
nick_hsieh <nick_hsieh@cht.com.tw>
2016-08-05 15:15:49 +0800

    Introduce end-of-line normalization

---
+++ WebDemo/Controllers/oAuthController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDemo.Controllers
+{
+    public class oAuthController : Controller
+    {
+        private string client_id = "";
+        private string client_secret = "";
+        private string redirect_uri = "";
+
+        // GET: oAuth
+        public ActionResult Index()
+        {
+            string Url = "https://www.facebook.com/dialog/oauth?scop={0}&redirect_url={1}&client_id={2}";
+
+            string scop = "email,read_stream";
+            string redirect_url_ecode = HttpUtility.UrlEncode(redirect_uri);
+
+            return View();
+        }
+    }
+}
