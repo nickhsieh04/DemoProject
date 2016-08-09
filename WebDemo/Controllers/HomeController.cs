f3c72e35ecb1c141d40cd456daa2c528ef3e5242
nick <nick.hsieh04@gmail.com>
2016-08-10 00:48:50 +0800

    增加Form登入機制

--- WebDemo/Controllers/HomeController.cs
+++ WebDemo/Controllers/HomeController.cs
@@ -4,32 +4,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using WebDemo.Common;
 using WebDemo.Models;
 
 namespace WebDemo.Contorllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        [CheckSessionTimeOut]
         public ActionResult Index()
         {
-            string token = "EAACEdEose0cBABTMrZBFnKwvwTZAZC3DhsdTeNYON0vQmLwfft5ki1bnZBinANa9gRXUbZAD8OXOF5hIfelxQMi6pwVvrOY4Dxrh1a5QzPgUrqfYSRHPHab8ZALb5y7zjBePVpqZABIzYMv7SN5ZBbyvIS8KDpxXyv3SPpujPooqoAZDZD";
-            //var client = new FacebookClient()
-            //{
-            //    AccessToken = token,
-            //    IsSecureConnection = true,
-            //    Version = "2.1"
-            //};
-            //dynamic fbresult = client.Get("4");
-            //FacebookUserModel facebookUser = Newtonsoft.Json.JsonConvert.DeserializeObject<FacebookUserModel>(fbresult.ToString());
+            var client = new FacebookClient()
+            {
+                AccessToken = Session["accessToken"].ToString()
+            };
+            client.Version = "v2.7";
+            dynamic fbresult = client.Get("me?fields=id,first_name,last_name,gender,locale,link,timezone,location,picture,age_range,name");
+            FacebookUserModel facebookUser = Newtonsoft.Json.JsonConvert.DeserializeObject<FacebookUserModel>(fbresult.ToString());
 
-            //var client = new FacebookClient();
-            //client.AccessToken = token;
+            return View(facebookUser);
+        }
 
-            //dynamic fbresult = client.Get("me?fields=id");
-
-
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
             return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Facebook()
+        {
+            var fb = new FacebookClient();
+            var loginUrl = fb.GetLoginUrl(new
+            {
+                client_id = System.Configuration.ConfigurationManager.AppSettings["FacebookAppId"],
+                client_secret = System.Configuration.ConfigurationManager.AppSettings["FacebookSecret"],
+                redirect_uri = RedirectUri.AbsoluteUri,
+                response_type = "code",
+                scope = "email", //Add other permissions as needed
+                display = "popup"
+            });
+
+            return Redirect(loginUrl.AbsoluteUri);
+        }
+
+        private Uri RedirectUri
+        {
+            get
+            {
+                var uriBuilder = new UriBuilder(Request.Url);
+                uriBuilder.Query = null;
+                uriBuilder.Fragment = null;
+                uriBuilder.Path = Url.Action("FacebookCallback");
+                return uriBuilder.Uri;
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult FacebookCallback(string code)
+        {
+            var fb = new FacebookClient();
+
+            dynamic result = fb.Post("oauth/access_token", new
+            {
+                client_id = System.Configuration.ConfigurationManager.AppSettings["FacebookAppId"],
+                client_secret = System.Configuration.ConfigurationManager.AppSettings["FacebookSecret"],
+                redirect_uri = RedirectUri.AbsoluteUri,
+                code = code
+            });
+
+            var accessToken = result.access_token;
+
+            // TODO: Authenticate User
+
+            // Store the access token in the session
+            Session["accessToken"] = accessToken;
+
+            // update the facebook client with the access token so 
+            // we can make requests on behalf of the user
+            fb.AccessToken = accessToken;
+
+            dynamic me = fb.Get("me?fields=first_name,last_name,id,email");
+            string email = me.email;
+
+            // Set the auth cookie
+            FormsAuthentication.SetAuthCookie(email, false);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Login");
         }
     }
 }
