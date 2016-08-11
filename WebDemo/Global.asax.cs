6938e97015fc6ca2b43df120428776e38c44c5d6
nick <nick.hsieh04@gmail.com>
2016-08-11 23:57:49 +0800

    Session Timeout 測試

--- WebDemo/Global.asax.cs
+++ WebDemo/Global.asax.cs
@@ -21,6 +21,8 @@ namespace WebDemo
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            
         }
 
         protected void Application_EndRequest()
